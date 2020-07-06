c0bf366867d2367a1d3d5f5edc3db64122a45c75
Levi Clancy <32032952+leviclancy@users.noreply.github.com>
2020-07-06 10:57:08 -0700

    Update index.php

--- index.php
+++ index.php
@@ -75,9 +75,50 @@ $sitemap_array = [
 		],
 	];
 	
+function print_navigation($navigation_link, $navigation_content) {
+
+	// The $navigation_content must have these language options
+	$language_options = ["he", "ku", "en"];
+	
+	// If any language options are missing then skip it
+//	foreach ($language_options as $option_temp):
+//		if (empty($navigation_content[$option_temp])): return; endif;
+//		endforeach;
+	
+	// Make sure the indent count is numeric
+	if (!(is_numeric($indent_count))): $indent_count = 0; endif;
+	
+	// Begin the navigation item
+	echo "<li><a href='/?link=". $navigation_link ."'>
+		
+	// Echo out the content of the navigation item
+	echo "<span class='navigation-item-content he'>". $navigation_content['he'] ."</span>";
+	echo "<span class='navigation-item-content ku'>". $navigation_content['ku'] ."</span>";
+	echo "<span class='navigation-item-content en'>". $navigation_content['en'] ."</span>";
+	
+	// Close the navigatio item
+	echo "</a>";
+	
+	// If no subpages, just close it here
+	if (empty($navigation_content['subpages'])):
+		echo "</li>"; endif;
+	
+	// If there are subpages, then keep going
+	foreach ($navigation_content['subpages'] as $navigation_link => $navigation_content):
+		echo "<ul>";
+		print_navigation($navigation_link, $navigation_content);
+		echo "</ul>";
+		endforeach;
+	
+	}
+
 ?>
 
 
 <div>
-
+<ul>
+<? foreach ($sitemap_array as $navigation_link => $navigation_content):
+	print_navigation($navigation_link, $navigation_content);
+	endforeach;
+</ul>
 </div>
