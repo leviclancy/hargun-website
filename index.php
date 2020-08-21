44ecc43e4597d52a061bcdc2908e5dfea1557f6d
Levi Clancy <32032952+leviclancy@users.noreply.github.com>
2020-08-21 15:22:31 -0700

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?
 
 include_once('configuration.php');
+include_once('translatable-elements.php');
 
 if (!(empty($unlock_key))):
 
@@ -11,362 +12,75 @@ if (!(empty($unlock_key))):
 
 	endif;
 
+$language_request_allowed = [ "ar"=>"عربي", "en"=>"English", "he"=>"עברית", "ku"=>"کوردی", ];
+$language_request = ( empty($_REQUEST['language']) ? "en" : $_REQUEST['language'] );
+if (!(isset($language_request_allowed[$language_request]))): $language_request = "en"; endif;
+
 $sitemap_array = [
- 	"about" => [
-		"he"=>"על הארגון",
-		"ku"=>"",
-		"ar"=>"",
-		"en"=>"National Association",
-		],
-	"history" => [
-		"he"=>"תולדות",
-		"ku"=>"",
-		"ar"=>"",
-		"en"=>"History",
-		],
+ 	"about" => [],
+	"history" => [],
 	"communities" => [
-		"he"=>"קהילות",
-		"ku"=>"",
-		"ar"=>"",
-		"en"=>"Communities",
-		"subpages"=>[
-			"urmia" => [
-				"he"=>"אורמיה-ריזאיה",
-				"ku"=>"",
-				"ar"=>"",
-				"en"=>"Urmia-Rezaiyeh",
-				],
-			"urfa" => [
-				"he"=>"אורפה",
-				"ku"=>"",
-				"ar"=>"",
-				"en"=>"Urfa",
-				],
-			"arbil" => [
-				"he"=>"ארביל",
-				"ku"=>"",
-				"ar"=>"",
-				"en"=>"Erbil",
-				],
-			"betanure" => [
-				"he"=>"ביתנורי",
-				"ku"=>"",
-				"ar"=>"",
-				"en"=>"Betanure",
-				],
-			"barashe" => [
-				"he"=>"בראשי",
-				"ku"=>"",
-				"ar"=>"",
-				"en"=>"Berashe",
-				],
-			"barzan" => [
-				"he"=>"ברזאן",
-				"ku"=>"",
-				"ar"=>"",
-				"en"=>"Barzan",
-				],
-			"duhok" => [
-				"he"=>"דוהוכ",
-				"ku"=>"دهۆک",
-				"ar"=>"",
-				"en"=>"Duhok",
-				],
-			"zakho" => [
-				"he"=>"זאכו",
-				"ku"=>"زاخۆ",
-				"ar"=>"زاخو",
-				"en"=>"Zakho",
-				],
-			"kirkuk" => [
-				"he"=>"כרכוכ",
-				"ku"=>"کرکوک",
-				"ar"=>"کرکوک",
-				"en"=>"Kirkuk",
-				],
-			"nerwa" => [
-				"he"=>"נירווא",
-				"ku"=>"",
-				"ar"=>"",
-				"en"=>"Nerwa",
-				],
-			"sondor" => [
-				"he"=>"נירווא",
-				"ku"=>"",
-				"ar"=>"",
-				"en"=>"Sondor",
-				],
-			"silemaniye" => [
-				"he"=>"סלימניה",
-				"ku"=>"سلێمانی",
-				"ar"=>"سلێمانی",
-				"en"=>"Silemaniye",
-				],
-			"amadiya" => [
-				"he"=>"עמדיה",
-				"ku"=>"",
-				"ar"=>"",
-				"en"=>"Amadiya",
-				],
-			"aqra" => [
-				"he"=>"עקרא",
-				"ku"=>"ئاکرێ",
-				"ar"=>"عقرة",
-				"en"=>"Aqra",
-				],
-			"challa" => [
-				"he"=>"צ'לא",
-				"ku"=>"",
-				"ar"=>"",
-				"en"=>"Challa",
-				],
-			"qamishli" => [
-				"he"=>"קמישלי",
-				"ku"=>"قامشلۆ",
-				"ar"=>"القامشلي",
-				"en"=>"Qamishli",
-				],
-			],
+		"urmia",
+		"urfa",
+		"arbil",
+		"betanure",
+		"barashe",
+		"barzan",
+		"duhok",
+		"zakho",
+		"kirkuk",
+		"nerwa",
+		"sondor",
+		"silemaniye",
+		"amadiya",
+		"aqra", 
+		"challa",
+		"qamishli",
 		],
 	"figures" => [
-		"he"=>"דמויות ואישים",
-		"ku"=>"",
-		"ar"=>"",
-		"en"=>"Figures",
-		"subpages"=> [
-			"rabbi-zakharia-berashe" => [
-				"he"=>"הרב זכריה בראשי",
-				"ku"=>"",
-				"ar"=>"",
-				"en"=>"Rabbi Zakharia Berashe",
-				],
-			"shimoni-habib" => [
-				"he"=>"שמעוני חביב",
-				"ku"=>"",
-				"ar"=>"",
-				"en"=>"Shimoni Habib",
-				],
-			"avidani-alwan" => [
-				"he"=>"אבידני עלואן",
-				"ku"=>"",
-				"ar"=>"",
-				"en"=>"",
-				],
-			"gershon-meir" => [
-				"he"=>"גרשון מאיר",
-				"ku"=>"",
-				"ar"=>"",
-				"en"=>"Gershon Meir",
-				],
-			"rabbi-shmuel-barukh" => [
-				"he"=>"הרב שמואל ברוך",
-				"ku"=>"",
-				"ar"=>"",
-				"en"=>"Shmuel Barukh",
-				],
-			"yitzhak-amedi" => [
-				"he"=>"יצחק עמדי",
-				"ku"=>"",
-				"ar"=>"",
-				"en"=>"Yitzhak Amedi",
-				],
-			"yitzhak-berashi" => [
-				"he"=>"יצחק בראשי",
-				"ku"=>"",
-				"ar"=>"",
-				"en"=>"Yitzhak Berashi",
-				],
-			"gabay-yitzhak" => [
-				"he"=>"גבאי יצחק",
-				"ku"=>"",
-				"ar"=>"",
-				"en"=>"Gabay Yitzhak",
-				],
-			"gabay-moshe" => [
-				"he"=>"גבאי משה",
-				"ku"=>"",
-				"ar"=>"",
-				"en"=>"Gabay Moshe",
-				],
-			"hacham-zaken-abraham" => [
-				"he"=>"חכם זקן אברהם",
-				"ku"=>"",
-				"ar"=>"",
-				"en"=>"Hacham Zaken Abraham",
-				],
-			"moshe-sinduri" => [
-				"he"=>"משה סנדורי",
-				"ku"=>"",
-				"ar"=>"",
-				"en"=>"Moshe Sinduri",
-				],
-			"hacham-habib-alvan" => [
-				"he"=>"חכם חביב עלון",
-				"ku"=>"",
-				"ar"=>"",
-				"en"=>"Hacham Habi Alvan",
-				],
-			"amiram-levi" => [
-				"he"=>"עמרם לוי",
-				"ku"=>"",
-				"ar"=>"",
-				"en"=>"Amiram Levi",
-				],
-			"hacham-mordechai-shabtay-zibrikho" => [
-				"he"=>"חכם מרדכי שבתאי זיבריכו",
-				"ku"=>"",
-				"ar"=>"",
-				"en"=>"Hacham Mordechai Shabtay Zibrikho",
-				],
-			"hacham-shalom-yacov" => [
-				"he"=>"חכם שלום יעקב",
-				"ku"=>"",
-				"ar"=>"",
-				"en"=>"Hacham Shalon Yacov",
-				],
-			"yacov-ahiya-hashiloni" => [
-				"he"=>"יעקב אחיה השילוני",
-				"ku"=>"",
-				"ar"=>"",
-				"en"=>"Yacov Ahiya HaShiloni",
-				],
-			"shimoni-shalom" => [
-				"he"=>"שמעוני שלום",
-				"ku"=>"",
-				"ar"=>"",
-				"en"=>"Shimoni Shalom",
-				],
-			"alvan-moshe" => [
-				"he"=>"עלון משה",
-				"ku"=>"",
-				"ar"=>"",
-				"en"=>"Alvan Moshe",
-				],
-			"nakhum-khaftsadi" => [
-				"he"=>"נחום חפצדה",
-				"ku"=>"",
-				"ar"=>"",
-				"en"=>"Nakhum Khaftsadi",
-				],
-			"hacham-yacov-babekha" => [
-				"he"=>"חכם יעקב בבכא",
-				"ku"=>"",
-				"ar"=>"",
-				"en"=>"Hacham Yacov Babekha",
-				],
-			"hacham-yosef-alvan" => [
-				"he"=>"חכם יוסף עלון",
-				"ku"=>"",
-				"ar"=>"",
-				"en"=>"Hacham Yosef Alvan",
-				],
-			"eliyahu-meir" => [
-				"he"=>"אלפייה מאיר",
-				"ku"=>"",
-				"ar"=>"",
-				"en"=>"Eliyahu Meir",
-				],
-			],
+		"rabbi-zakharia-berashe",
+		"shimoni-habib",
+		"avidani-alwan",
+		"gershon-meir",
+		"rabbi-shmuel-barukh",
+		"yitzhak-amedi",
+		"yitzhak-berashi",
+		"gabay-yitzhak",
+		"gabay-moshe",
+		"hacham-zaken-abraham",
+		"moshe-sinduri",
+		"hacham-habib-alvan",
+		"amiram-levi",
+		"hacham-mordechai-shabtay-zibrikho",
+		"hacham-shalom-yacov",
+		"yacov-ahiya-hashiloni",
+		"shimoni-shalom",
+		"alvan-moshe",
+		"nakhum-khaftsadi",
+		"hacham-yacov-babekha",
+		"hacham-yosef-alvan",
+		"eliyahu-meir",
 		],
 	"circle-of-life" => [
-		"he"=>"מחזור החיים",
-		"ku"=>"",
-		"ar"=>"",
-		"en"=>"Circle of Life",
-		"subpages"=>[
-			"wedding" => [
-				"he"=>"חתונה",
-				"ku"=>"",
-				"ar"=>"",
-				"en"=>"Wedding",
-				],
-			"childbirth" => [
-				"he"=>"הולדת בנים",
-				"ku"=>"",
-				"ar"=>"",
-				"en"=>"Childbirth",
-				],
-			"bar-mitzvah" => [
-				"he"=>"בר-מצווה",
-				"ku"=>"",
-				"ar"=>"",
-				"en"=>"Bar Mitzvah",
-				],
-			"funerary" => [
-				"he"=>"פטירה",
-				"ku"=>"",
-				"ar"=>"",
-				"en"=>"Funerary",
-				],
-			],
-
+		"wedding",
+		"childbirth",
+		"bar-mitzvah",
+		"funerary",
 		],
-	"folklore" => [
-		"he"=>"פולקלור",
-		"ku"=>"",
-		"ar"=>"",
-		"en"=>"Folklore",
-		"subpages"=> [
-			"music" => [
-				"he"=>"מוסיקה",
-				"ku"=>"",
-				"ar"=>"",
-				"en"=>"Music",
-				],
-			"" => [
-				"he"=>"",
-				"ku"=>"",
-				"ar"=>"",
-				"en"=>"",
-				],
-			"" => [
-				"he"=>"",
-				"ku"=>"",
-				"ar"=>"",
-				"en"=>"",
-				],
-			"" => [
-				"he"=>"",
-				"ku"=>"",
-				"ar"=>"",
-				"en"=>"",
-				],
-			],
+	"folklore" =>
+		"music",
 		],
-	"publications" => [
-		"he"=>"ספרים וכתבי - עת",
-		"ku"=>"",
-		"ar"=>"",
-		"en"=>"Publications",
-		],
-	"recipes" => [
-		"he"=>"מהמטבח הכורדי",
-		"ku"=>"",
-		"ar"=>"",
-		"en"=>"Recipes",
-		],
-	"photos" => [
-		"he"=>"ארכיון תמונות",
-		"ku"=>"",
-		"ar"=>"",
-		"en"=>"Photo archive",
-		],
-	"events" => [
-		"he"=>"אירועים",
-		"ku"=>"",
-		"ar"=>"",
-		"en"=>"Events",
-		],
-	"videos" => [
-		"he"=>"סרטים",
-		"ku"=>"",
-		"ar"=>"",
-		"en"=>"Videos",
-		],
+	"publications" => [],
+	"recipes" => [],
+	"photos" => [],
+	"events" => [],
+	"videos" => [],
 	];
 	
 function print_navigation($navigation_link, $navigation_content) {
 
+	global $translatable_elements;
+	
 	// The $navigation_content must have these language options
 	$language_options = ["he", "ku", "en"];
 	
@@ -456,7 +170,15 @@ a {
 <div id='navigation-sidebar'>
 <ul>
 <? foreach ($sitemap_array as $navigation_link => $navigation_content):
-	print_navigation($navigation_link, $navigation_content);
+	echo "<li><a href='". $navigation_link ."'>". $translatable_elements[$language_request][$navigation_link] ."</a>";
+	if (!(empty($navigation_content))): 
+		echo "<ul>";
+		foreach ($navigaton_content as $subnavigation_link => $subnavigation_content):
+			echo "<li><a href='". $navigation_link ."'>". $translatable_elements[$language_request][$navigation_link] ."</a></li>";
+			endforeach;
+		echo "</ul>";
+		endif;
+	echo "</li>";
 	endforeach; ?>
 </ul>
 </div>
