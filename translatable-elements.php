095d8d6f3696e4dc6756c5d65fbcf5297d701440
Levi Clancy <32032952+leviclancy@users.noreply.github.com>
2020-08-21 15:17:41 -0700

    Update translatable-elements.php

--- translatable-elements.php
+++ translatable-elements.php
@@ -1,4 +1,5 @@
 <?
+
 $translatable_elemnts = [
  	"about" => [
 		"he"=>"על הארגון",
@@ -6,345 +7,370 @@ $translatable_elemnts = [
 		"ar"=>"",
 		"en"=>"National Association",
 		],
+	
 	"history" => [
 		"he"=>"תולדות",
 		"ku"=>"",
 		"ar"=>"",
 		"en"=>"History",
 		],
+	
 	"communities" => [
 		"he"=>"קהילות",
 		"ku"=>"",
 		"ar"=>"",
 		"en"=>"Communities",
-		"subpages"=>[
-			"urmia" => [
-				"he"=>"אורמיה-ריזאיה",
-				"ku"=>"",
-				"ar"=>"",
-				"en"=>"Urmia-Rezaiyeh",
-				],
-			"urfa" => [
-				"he"=>"אורפה",
-				"ku"=>"",
-				"ar"=>"",
-				"en"=>"Urfa",
-				],
-			"arbil" => [
-				"he"=>"ארביל",
-				"ku"=>"",
-				"ar"=>"",
-				"en"=>"Erbil",
-				],
-			"betanure" => [
-				"he"=>"ביתנורי",
-				"ku"=>"",
-				"ar"=>"",
-				"en"=>"Betanure",
-				],
-			"barashe" => [
-				"he"=>"בראשי",
-				"ku"=>"",
-				"ar"=>"",
-				"en"=>"Berashe",
-				],
-			"barzan" => [
-				"he"=>"ברזאן",
-				"ku"=>"",
-				"ar"=>"",
-				"en"=>"Barzan",
-				],
-			"duhok" => [
-				"he"=>"דוהוכ",
-				"ku"=>"دهۆک",
-				"ar"=>"",
-				"en"=>"Duhok",
-				],
-			"zakho" => [
-				"he"=>"זאכו",
-				"ku"=>"زاخۆ",
-				"ar"=>"زاخو",
-				"en"=>"Zakho",
-				],
-			"kirkuk" => [
-				"he"=>"כרכוכ",
-				"ku"=>"کرکوک",
-				"ar"=>"کرکوک",
-				"en"=>"Kirkuk",
-				],
-			"nerwa" => [
-				"he"=>"נירווא",
-				"ku"=>"",
-				"ar"=>"",
-				"en"=>"Nerwa",
-				],
-			"sondor" => [
-				"he"=>"נירווא",
-				"ku"=>"",
-				"ar"=>"",
-				"en"=>"Sondor",
-				],
-			"silemaniye" => [
-				"he"=>"סלימניה",
-				"ku"=>"سلێمانی",
-				"ar"=>"سلێمانی",
-				"en"=>"Silemaniye",
-				],
-			"amadiya" => [
-				"he"=>"עמדיה",
-				"ku"=>"",
-				"ar"=>"",
-				"en"=>"Amadiya",
-				],
-			"aqra" => [
-				"he"=>"עקרא",
-				"ku"=>"ئاکرێ",
-				"ar"=>"عقرة",
-				"en"=>"Aqra",
-				],
-			"challa" => [
-				"he"=>"צ'לא",
-				"ku"=>"",
-				"ar"=>"",
-				"en"=>"Challa",
-				],
-			"qamishli" => [
-				"he"=>"קמישלי",
-				"ku"=>"قامشلۆ",
-				"ar"=>"القامشلي",
-				"en"=>"Qamishli",
-				],
-			],
+
+	"urmia" => [
+		"he"=>"אורמיה-ריזאיה",
+		"ku"=>"",
+		"ar"=>"",
+		"en"=>"Urmia-Rezaiyeh",
 		],
+
+	"urfa" => [
+		"he"=>"אורפה",
+		"ku"=>"",
+		"ar"=>"",
+		"en"=>"Urfa",
+		],
+		
+	"arbil" => [
+		"he"=>"ארביל",
+		"ku"=>"",
+		"ar"=>"",
+		"en"=>"Erbil",
+		],
+
+	"betanure" => [
+		"he"=>"ביתנורי",
+		"ku"=>"",
+		"ar"=>"",
+		"en"=>"Betanure",
+		],
+
+	"barashe" => [
+		"he"=>"בראשי",
+		"ku"=>"",
+		"ar"=>"",
+		"en"=>"Berashe",
+		],
+
+	"barzan" => [
+		"he"=>"ברזאן",
+		"ku"=>"",
+		"ar"=>"",
+		"en"=>"Barzan",
+		],
+
+	"duhok" => [
+		"he"=>"דוהוכ",
+		"ku"=>"دهۆک",
+		"ar"=>"",
+		"en"=>"Duhok",
+		],
+
+	"zakho" => [
+		"he"=>"זאכו",
+		"ku"=>"زاخۆ",
+		"ar"=>"زاخو",
+		"en"=>"Zakho",
+		],
+	
+	"kirkuk" => [
+		"he"=>"כרכוכ",
+		"ku"=>"کرکوک",
+		"ar"=>"کرکوک",
+		"en"=>"Kirkuk",
+		],
+
+	"nerwa" => [
+		"he"=>"נירווא",
+		"ku"=>"",
+		"ar"=>"",
+		"en"=>"Nerwa",
+		],
+
+	"sondor" => [
+		"he"=>"נירווא",
+		"ku"=>"",
+		"ar"=>"",
+		"en"=>"Sondor",
+		],
+		
+	"silemaniye" => [
+		"he"=>"סלימניה",
+		"ku"=>"سلێمانی",
+		"ar"=>"سلێمانی",
+		"en"=>"Silemaniye",
+		],
+		
+	"amadiya" => [
+		"he"=>"עמדיה",
+		"ku"=>"",
+		"ar"=>"",
+		"en"=>"Amadiya",
+		],
+
+	"aqra" => [
+		"he"=>"עקרא",
+		"ku"=>"ئاکرێ",
+		"ar"=>"عقرة",
+		"en"=>"Aqra",
+		],
+
+	"challa" => [
+		"he"=>"צ'לא",
+		"ku"=>"",
+		"ar"=>"",
+		"en"=>"Challa",
+		],
+
+	"qamishli" => [
+		"he"=>"קמישלי",
+		"ku"=>"قامشلۆ",
+		"ar"=>"القامشلي",
+		"en"=>"Qamishli",
+		],
+
 	"figures" => [
 		"he"=>"דמויות ואישים",
 		"ku"=>"",
 		"ar"=>"",
 		"en"=>"Figures",
-		"subpages"=> [
-			"rabbi-zakharia-berashe" => [
-				"he"=>"הרב זכריה בראשי",
-				"ku"=>"",
-				"ar"=>"",
-				"en"=>"Rabbi Zakharia Berashe",
-				],
-			"shimoni-habib" => [
-				"he"=>"שמעוני חביב",
-				"ku"=>"",
-				"ar"=>"",
-				"en"=>"Shimoni Habib",
-				],
-			"avidani-alwan" => [
-				"he"=>"אבידני עלואן",
-				"ku"=>"",
-				"ar"=>"",
-				"en"=>"",
-				],
-			"gershon-meir" => [
-				"he"=>"גרשון מאיר",
-				"ku"=>"",
-				"ar"=>"",
-				"en"=>"Gershon Meir",
-				],
-			"rabbi-shmuel-barukh" => [
-				"he"=>"הרב שמואל ברוך",
-				"ku"=>"",
-				"ar"=>"",
-				"en"=>"Shmuel Barukh",
-				],
-			"yitzhak-amedi" => [
-				"he"=>"יצחק עמדי",
-				"ku"=>"",
-				"ar"=>"",
-				"en"=>"Yitzhak Amedi",
-				],
-			"yitzhak-berashi" => [
-				"he"=>"יצחק בראשי",
-				"ku"=>"",
-				"ar"=>"",
-				"en"=>"Yitzhak Berashi",
-				],
-			"gabay-yitzhak" => [
-				"he"=>"גבאי יצחק",
-				"ku"=>"",
-				"ar"=>"",
-				"en"=>"Gabay Yitzhak",
-				],
-			"gabay-moshe" => [
-				"he"=>"גבאי משה",
-				"ku"=>"",
-				"ar"=>"",
-				"en"=>"Gabay Moshe",
-				],
-			"hacham-zaken-abraham" => [
-				"he"=>"חכם זקן אברהם",
-				"ku"=>"",
-				"ar"=>"",
-				"en"=>"Hacham Zaken Abraham",
-				],
-			"moshe-sinduri" => [
-				"he"=>"משה סנדורי",
-				"ku"=>"",
-				"ar"=>"",
-				"en"=>"Moshe Sinduri",
-				],
-			"hacham-habib-alvan" => [
-				"he"=>"חכם חביב עלון",
-				"ku"=>"",
-				"ar"=>"",
-				"en"=>"Hacham Habi Alvan",
-				],
-			"amiram-levi" => [
-				"he"=>"עמרם לוי",
-				"ku"=>"",
-				"ar"=>"",
-				"en"=>"Amiram Levi",
-				],
-			"hacham-mordechai-shabtay-zibrikho" => [
-				"he"=>"חכם מרדכי שבתאי זיבריכו",
-				"ku"=>"",
-				"ar"=>"",
-				"en"=>"Hacham Mordechai Shabtay Zibrikho",
-				],
-			"hacham-shalom-yacov" => [
-				"he"=>"חכם שלום יעקב",
-				"ku"=>"",
-				"ar"=>"",
-				"en"=>"Hacham Shalon Yacov",
-				],
-			"yacov-ahiya-hashiloni" => [
-				"he"=>"יעקב אחיה השילוני",
-				"ku"=>"",
-				"ar"=>"",
-				"en"=>"Yacov Ahiya HaShiloni",
-				],
-			"shimoni-shalom" => [
-				"he"=>"שמעוני שלום",
-				"ku"=>"",
-				"ar"=>"",
-				"en"=>"Shimoni Shalom",
-				],
-			"alvan-moshe" => [
-				"he"=>"עלון משה",
-				"ku"=>"",
-				"ar"=>"",
-				"en"=>"Alvan Moshe",
-				],
-			"nakhum-khaftsadi" => [
-				"he"=>"נחום חפצדה",
-				"ku"=>"",
-				"ar"=>"",
-				"en"=>"Nakhum Khaftsadi",
-				],
-			"hacham-yacov-babekha" => [
-				"he"=>"חכם יעקב בבכא",
-				"ku"=>"",
-				"ar"=>"",
-				"en"=>"Hacham Yacov Babekha",
-				],
-			"hacham-yosef-alvan" => [
-				"he"=>"חכם יוסף עלון",
-				"ku"=>"",
-				"ar"=>"",
-				"en"=>"Hacham Yosef Alvan",
-				],
-			"eliyahu-meir" => [
-				"he"=>"אלפייה מאיר",
-				"ku"=>"",
-				"ar"=>"",
-				"en"=>"Eliyahu Meir",
-				],
-			],
 		],
+		
+	"rabbi-zakharia-berashe" => [
+		"he"=>"הרב זכריה בראשי",
+		"ku"=>"",
+		"ar"=>"",
+		"en"=>"Rabbi Zakharia Berashe",
+		],
+
+	"shimoni-habib" => [
+		"he"=>"שמעוני חביב",
+		"ku"=>"",
+		"ar"=>"",
+		"en"=>"Shimoni Habib",
+		],
+
+	"avidani-alwan" => [
+		"he"=>"אבידני עלואן",
+		"ku"=>"",
+		"ar"=>"",
+		"en"=>"",
+		],
+
+	"gershon-meir" => [
+		"he"=>"גרשון מאיר",
+		"ku"=>"",
+		"ar"=>"",
+		"en"=>"Gershon Meir",
+		],
+
+	"rabbi-shmuel-barukh" => [
+		"he"=>"הרב שמואל ברוך",
+		"ku"=>"",
+		"ar"=>"",
+		"en"=>"Shmuel Barukh",
+		],
+
+	"yitzhak-amedi" => [
+		"he"=>"יצחק עמדי",
+		"ku"=>"",
+		"ar"=>"",
+		"en"=>"Yitzhak Amedi",
+		],
+
+	"yitzhak-berashi" => [
+		"he"=>"יצחק בראשי",
+		"ku"=>"",
+		"ar"=>"",
+		"en"=>"Yitzhak Berashi",
+		],
+
+	"gabay-yitzhak" => [
+		"he"=>"גבאי יצחק",
+		"ku"=>"",
+		"ar"=>"",
+		"en"=>"Gabay Yitzhak",
+		],
+
+	"gabay-moshe" => [
+		"he"=>"גבאי משה",
+		"ku"=>"",
+		"ar"=>"",
+		"en"=>"Gabay Moshe",
+		],
+
+	"hacham-zaken-abraham" => [
+		"he"=>"חכם זקן אברהם",
+		"ku"=>"",
+		"ar"=>"",
+		"en"=>"Hacham Zaken Abraham",
+		],
+
+	"moshe-sinduri" => [
+		"he"=>"משה סנדורי",
+		"ku"=>"",
+		"ar"=>"",
+		"en"=>"Moshe Sinduri",
+		],
+
+	"hacham-habib-alvan" => [
+		"he"=>"חכם חביב עלון",
+		"ku"=>"",
+		"ar"=>"",
+		"en"=>"Hacham Habi Alvan",
+		],
+
+	"amiram-levi" => [
+		"he"=>"עמרם לוי",
+		"ku"=>"",
+		"ar"=>"",
+		"en"=>"Amiram Levi",
+		],
+
+	"hacham-mordechai-shabtay-zibrikho" => [
+		"he"=>"חכם מרדכי שבתאי זיבריכו",
+		"ku"=>"",
+		"ar"=>"",
+		"en"=>"Hacham Mordechai Shabtay Zibrikho",
+		],
+
+	"hacham-shalom-yacov" => [
+		"he"=>"חכם שלום יעקב",
+		"ku"=>"",
+		"ar"=>"",
+		"en"=>"Hacham Shalon Yacov",
+		],
+
+	"yacov-ahiya-hashiloni" => [
+		"he"=>"יעקב אחיה השילוני",
+		"ku"=>"",
+		"ar"=>"",
+		"en"=>"Yacov Ahiya HaShiloni",
+		],
+
+	"shimoni-shalom" => [
+		"he"=>"שמעוני שלום",
+		"ku"=>"",
+		"ar"=>"",
+		"en"=>"Shimoni Shalom",
+		],
+
+	"alvan-moshe" => [
+		"he"=>"עלון משה",
+		"ku"=>"",
+		"ar"=>"",
+		"en"=>"Alvan Moshe",
+		],
+
+	"nakhum-khaftsadi" => [
+		"he"=>"נחום חפצדה",
+		"ku"=>"",
+		"ar"=>"",
+		"en"=>"Nakhum Khaftsadi",
+		],
+
+	"hacham-yacov-babekha" => [
+		"he"=>"חכם יעקב בבכא",
+		"ku"=>"",
+		"ar"=>"",
+		"en"=>"Hacham Yacov Babekha",
+		],
+
+	"hacham-yosef-alvan" => [
+		"he"=>"חכם יוסף עלון",
+		"ku"=>"",
+		"ar"=>"",
+		"en"=>"Hacham Yosef Alvan",
+		],
+
+	"eliyahu-meir" => [
+		"he"=>"אלפייה מאיר",
+		"ku"=>"",
+		"ar"=>"",
+		"en"=>"Eliyahu Meir",
+		],
+		
 	"circle-of-life" => [
 		"he"=>"מחזור החיים",
 		"ku"=>"",
 		"ar"=>"",
 		"en"=>"Circle of Life",
-		"subpages"=>[
-			"wedding" => [
-				"he"=>"חתונה",
-				"ku"=>"",
-				"ar"=>"",
-				"en"=>"Wedding",
-				],
-			"childbirth" => [
-				"he"=>"הולדת בנים",
-				"ku"=>"",
-				"ar"=>"",
-				"en"=>"Childbirth",
-				],
-			"bar-mitzvah" => [
-				"he"=>"בר-מצווה",
-				"ku"=>"",
-				"ar"=>"",
-				"en"=>"Bar Mitzvah",
-				],
-			"funerary" => [
-				"he"=>"פטירה",
-				"ku"=>"",
-				"ar"=>"",
-				"en"=>"Funerary",
-				],
-			],
-
 		],
+		
+	"wedding" => [
+		"he"=>"חתונה",
+		"ku"=>"",
+		"ar"=>"",
+		"en"=>"Wedding",
+		],
+
+	"childbirth" => [
+		"he"=>"הולדת בנים",
+		"ku"=>"",
+		"ar"=>"",
+		"en"=>"Childbirth",
+		],
+		
+	"bar-mitzvah" => [
+		"he"=>"בר-מצווה",
+		"ku"=>"",
+		"ar"=>"",
+		"en"=>"Bar Mitzvah",
+		],
+
+	"funerary" => [
+		"he"=>"פטירה",
+		"ku"=>"",
+		"ar"=>"",
+		"en"=>"Funerary",
+		],
+
 	"folklore" => [
 		"he"=>"פולקלור",
 		"ku"=>"",
 		"ar"=>"",
 		"en"=>"Folklore",
-		"subpages"=> [
-			"music" => [
-				"he"=>"מוסיקה",
-				"ku"=>"",
-				"ar"=>"",
-				"en"=>"Music",
-				],
-			"" => [
-				"he"=>"",
-				"ku"=>"",
-				"ar"=>"",
-				"en"=>"",
-				],
-			"" => [
-				"he"=>"",
-				"ku"=>"",
-				"ar"=>"",
-				"en"=>"",
-				],
-			"" => [
-				"he"=>"",
-				"ku"=>"",
-				"ar"=>"",
-				"en"=>"",
-				],
-			],
 		],
+
+	"music" => [
+		"he"=>"מוסיקה",
+		"ku"=>"",
+		"ar"=>"",
+		"en"=>"Music",
+		],
+
 	"publications" => [
 		"he"=>"ספרים וכתבי - עת",
 		"ku"=>"",
 		"ar"=>"",
 		"en"=>"Publications",
 		],
+
 	"recipes" => [
 		"he"=>"מהמטבח הכורדי",
 		"ku"=>"",
 		"ar"=>"",
 		"en"=>"Recipes",
 		],
+
 	"photos" => [
 		"he"=>"ארכיון תמונות",
 		"ku"=>"",
 		"ar"=>"",
 		"en"=>"Photo archive",
 		],
+
 	"events" => [
 		"he"=>"אירועים",
 		"ku"=>"",
 		"ar"=>"",
 		"en"=>"Events",
 		],
+
 	"videos" => [
 		"he"=>"סרטים",
 		"ku"=>"",
