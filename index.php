b0701d8473fb21b40d425f0e8133fb6509e83979
Levi Clancy <32032952+leviclancy@users.noreply.github.com>
2020-08-21 15:40:47 -0700

    Update index.php

--- index.php
+++ index.php
@@ -92,10 +92,14 @@ function language_picker($string_id, $language_command=null) {
 	
 	if (empty($language_command) || !(isset($language_request_allowed[$language_command]))): $language_command = $language_request; endif;
 	
-	if (!(empty($translatable_elements[$string_id][$language_command]))): echo $translatable_elements[$string_id][$language_command]; endif;
+	if (!(empty($translatable_elements[$string_id][$language_command]))):
+		echo $translatable_elements[$string_id][$language_command];
+		return; endif;
 	
 	foreach (array_keys($language_request_allowed) as $language_request_possible):
-		if (!(empty($translatable_elements[$string_id][$language_request_possible]))): echo $translatable_elements[$string_id][$language_request_possible]; endif;
+		if (!(empty($translatable_elements[$string_id][$language_request_possible]))):
+			echo $translatable_elements[$string_id][$language_request_possible];
+			return; endif;
 		endforeach;
 	
 	}
